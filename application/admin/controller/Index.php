d61d52ab68850bfe0c90ae9146f60b957374a8e9
Administrator <1880161@qq.com>
2017-11-12 01:41:30 +0800

    11

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -1,13 +1,23 @@
 <?php
 namespace app\admin\controller;
 
-class Index extends \think\Controller
+use app\admin\common\Base;
+
+class Index extends Base
 {
     public function index()
     {
         return $this->fetch();
     }
     public function welcome()
+    {
+        return $this->fetch();
+    }
+    public function questionList()
+    {
+        return $this->fetch();
+    }
+    public function question_del()
     {
         return $this->fetch();
     }
